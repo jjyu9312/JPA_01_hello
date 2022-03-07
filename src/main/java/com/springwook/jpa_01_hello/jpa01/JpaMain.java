85e39c8ec3e7fdef817079dc03eddc4c6eb524c3
kkw <byl9312@gmail.com>
2022-03-07 13:03:44 +0900

    22/03/07 영속성 관리

--- src/main/java/com/springwook/jpa_01_hello/jpa01/JpaMain.java
+++ src/main/java/com/springwook/jpa_01_hello/jpa01/JpaMain.java
@@ -42,6 +42,7 @@ public class JpaMain {
             em.remove(findMember);
             */
 
+            /*
             // Member 객체를 대상으로 조회
             List<Member> result = em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(1)
@@ -51,9 +52,47 @@ public class JpaMain {
             for (Member member : result) {
                 System.out.println("member.name = " + member.getName());
             }
+             */
 
+            /*
+            // 현재 비영속 상태
+            Member member = new Member();
+            member.setId(5L);
+            member.setName("3k");
+
+            // 영속 상태
+            System.out.println("==========BEFORE==========");
+            em.persist(member);
+            System.out.println("==========AFTER==========");
+             */
+
+            /*
+            Member findMember1 = em.find(Member.class, 4L);
+            Member findMember2 = em.find(Member.class, 4L);
+
+            System.out.println("findMember1.id = " + findMember1.getId());
+            System.out.println("findMember1.name = " + findMember1.getName());
+            System.out.println("======================================");
+            System.out.println("findMember1.id = " + findMember1.getId());
+            System.out.println("findMember1.name = " + findMember1.getName());
+             */
+
+            /*
+            // 영속
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(200L, "B");
+
+             em.persist(member1);
+             em.persist(member2);
+
+            System.out.println("=====================================");
+             */
+
+            Member findMember = em.find(Member.class, 150L);
+            findMember.setName("Z");
+
+            // em.persist(findMember); 해당 코드는 필요없음. 왜냐하면 Collections과 같다고 생각하면 됨
             tx.commit();
-
         } catch (Exception e) {
             tx.rollback();
 
--- src/main/java/com/springwook/jpa_01_hello/jpa01/Member.java
+++ src/main/java/com/springwook/jpa_01_hello/jpa01/Member.java
@@ -11,6 +11,13 @@ public class Member {
 
     private String name;
 
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Member() {};
+
     public Long getId() {
         return id;
     }
